65150377f85a51cfdc531ed1d1650028479daadf
Dmitriy Fokin <dmitrii.fokin@aisa.ru>
2021-01-06 23:48:07 +0300

    first commit. init empty project

---
+++ src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/ClientServiceViewApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.clientserviceview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientServiceViewApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClientServiceViewApplication>(*args)
+}
---
+++ src/test/kotlin/ru/fdo/bank/finrez/clientserviceview/ClientServiceViewApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.fdo.bank.finrez.clientserviceview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientServiceViewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
