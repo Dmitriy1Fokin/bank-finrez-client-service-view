40284d7e22029f056840ac69f338d06eed619e9c
Dmitriy Fokin <fokinds1@gmail.com>
2021-01-10 07:57:22 +0300

    added new get requests

--- src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/controller/ClientController.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/controller/ClientController.kt
@@ -1,8 +1,10 @@
 package ru.fdo.bank.finrez.clientserviceview.controller
 
+import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.fdo.bank.finrez.clientserviceview.domain.Client
 import ru.fdo.bank.finrez.clientserviceview.domain.ClientCorp
@@ -18,5 +20,20 @@ class ClientController(private val clientService: ClientService){
     @GetMapping("/{clientId}")
     fun getClient(@PathVariable("clientId") clientId: String) : CompletableFuture<Client> =
             clientService.getClintById(clientId)
+
+    @GetMapping("/all")
+    fun getAllClients(@RequestParam("page") page: Int,
+                      @RequestParam("size") size: Int) : CompletableFuture<List<Client>> =
+            clientService.getAllClients(page, size)
+
+    @GetMapping("/type")
+    fun getAllClientsByType(@RequestParam("typeOfClient") typeOfClient: TypeOfClient,
+                            @RequestParam("page") page: Int,
+                            @RequestParam("size") size: Int) : CompletableFuture<List<Client>> =
+            clientService.getAllClientsByType(typeOfClient, page, size)
+
+    @GetMapping("/search")
+    fun getClientsByParams(@RequestParam searchParams: Map<String, String>) : CompletableFuture<List<Client>> =
+            clientService.getClientsByParams(searchParams)
 
 }
