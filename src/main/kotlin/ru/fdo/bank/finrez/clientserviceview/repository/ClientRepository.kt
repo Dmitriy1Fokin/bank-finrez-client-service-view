e9c5bba618d13e0e3a22bebb66dcef32071723e8
Dmitriy Fokin <fokinds1@gmail.com>
2021-01-10 07:53:21 +0300

    added method findAllByTypeOfClient

--- src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/repository/ClientRepository.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/repository/ClientRepository.kt
@@ -1,6 +1,11 @@
 package ru.fdo.bank.finrez.clientserviceview.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.fdo.bank.finrez.clientserviceview.domain.Client
+import ru.fdo.bank.finrez.clientserviceview.domain.TypeOfClient
 
-interface ClientRepository : JpaRepository<Client, String>
+interface ClientRepository : JpaRepository<Client, String>{
+    fun findAllByTypeOfClient(typeOfClient: TypeOfClient, pageable: Pageable) : Page<Client>
+}
