e24c313327f50e0fd85a508539db226ce0954ae0
Dmitriy Fokin <fokinds1@gmail.com>
2021-01-12 13:31:24 +0300

    deleted unnecessary imports

--- src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/controller/ClientController.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/controller/ClientController.kt
@@ -1,15 +1,12 @@
 package ru.fdo.bank.finrez.clientserviceview.controller
 
-import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import ru.fdo.bank.finrez.clientserviceview.domain.Client
-import ru.fdo.bank.finrez.clientserviceview.domain.ClientCorp
 import ru.fdo.bank.finrez.clientserviceview.domain.TypeOfClient
-import ru.fdo.bank.finrez.clientserviceview.repository.ClientRepository
 import ru.fdo.bank.finrez.clientserviceview.service.ClientService
 import java.util.concurrent.CompletableFuture
 
