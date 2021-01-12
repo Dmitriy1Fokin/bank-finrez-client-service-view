5ff0039c5cea8eff28bdbe7f2b841341464e7a49
Dmitriy Fokin <fokinds1@gmail.com>
2021-01-12 13:46:45 +0300

    added implementation JpaSpecificationExecutor

--- src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/repository/ClientRepository.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/repository/ClientRepository.kt
@@ -3,9 +3,10 @@ package ru.fdo.bank.finrez.clientserviceview.repository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import ru.fdo.bank.finrez.clientserviceview.domain.Client
 import ru.fdo.bank.finrez.clientserviceview.domain.TypeOfClient
 
-interface ClientRepository : JpaRepository<Client, String>{
+interface ClientRepository : JpaRepository<Client, String>, JpaSpecificationExecutor<Client>{
     fun findAllByTypeOfClient(typeOfClient: TypeOfClient, pageable: Pageable) : Page<Client>
 }
