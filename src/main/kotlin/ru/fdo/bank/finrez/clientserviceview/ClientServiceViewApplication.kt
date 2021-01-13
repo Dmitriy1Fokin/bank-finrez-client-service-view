ccfc78292f97a1202507ca49a501526d98787872
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-13 09:04:00 +0300

    added discovery client (eureka)

--- src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/ClientServiceViewApplication.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientserviceview/ClientServiceViewApplication.kt
@@ -2,8 +2,10 @@ package ru.fdo.bank.finrez.clientserviceview
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 open class ClientServiceViewApplication
 
 fun main(args: Array<String>) {
